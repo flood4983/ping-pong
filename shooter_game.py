a527a70bc01274988c432287648a9198d0d08087
flood4983 <makszinnatullin10@gmail.com>
2021-03-27 19:25:19 +0500

    ракетки

--- shooter_game.py
+++ shooter_game.py
@@ -1,12 +1,9 @@
 from pygame import *
 from random import randint
 from time import time as timer
-life = 3
+
 #фоновая музыка
-mixer.init()
-mixer.music.load('space.ogg')
-mixer.music.play()
-fire_sound = mixer.Sound('fire.ogg')
+
 
 #шрифты и надписи
 font.init()
@@ -16,14 +13,11 @@ lose = font1.render('YOU LOSE!', True, (180, 0, 0))
 font2 = font.SysFont("Arial", 36)
 
 # нам нужны такие картинки:
-img_back = "galaxy.jpg" # фон игры
-img_hero = "rocket.png" # герой
-img_bullet = "bullet.png" # пуля
-img_enemy = "ufo.png" # враг
-img_aster = "aster.png"
-score = 0 # сбито кораблей
-lost = 0 # пропущено кораблей
-max_lost = 3 # проиграли, если пропустили столько
+
+img_back = "fon.jpg" #фон
+img_hero_l = "player_l.png"
+img_hero_r = "player_r.png" # герой
+
 
 # класс-родитель для других спрайтов
 class GameSprite(sprite.Sprite):
@@ -48,16 +42,19 @@ class GameSprite(sprite.Sprite):
 # класс главного игрока
 class Player(GameSprite):
     # метод для управления спрайтом стрелками клавиатуры
-    def update(self):
+    def uprav_r(self):
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < win_width - 80:
-            self.rect.x += self.speed
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
   # метод "выстрел" (используем место игрока, чтобы создать там пулю)
-    def fire(self):
-        bullet = Bullet(img_bullet, self.rect.centerx, self.rect.top, 15, 20, -15)
-        bullets.add(bullet)
+    def uprav_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
 
 class Aster(GameSprite):
     # движение врага
@@ -71,47 +68,21 @@ class Aster(GameSprite):
             
 
 # класс спрайта-врага   
-class Enemy(GameSprite):
-    # движение врага
-    def update(self):
-        self.rect.y += self.speed
-        global lost
-        # исчезает, если дойдет до края экрана
-        if self.rect.y > win_height:
-            self.rect.x = randint(80, win_width - 80)
-            self.rect.y = 0
-            lost = lost + 1
+
 
 # класс спрайта-пули   
-class Bullet(GameSprite):
-    # движение врага
-    def update(self):
-        self.rect.y += self.speed
-        # исчезает, если дойдет до края экрана
-        if self.rect.y < 0:
-            self.kill()
+
 
 # Создаем окошко
 win_width = 700
 win_height = 500
-display.set_caption("Shooter")
+display.set_caption("Ping Pong")
 window = display.set_mode((win_width, win_height))
 background = transform.scale(image.load(img_back), (win_width, win_height))
 
 # создаем спрайты
-ship = Player(img_hero, 5, win_height - 100, 80, 100, 10)
-
-monsters = sprite.Group()
-for i in range(1, 6):
-    monster = Enemy(img_enemy, randint(80, win_width - 80), -40, 80, 50, randint(1, 5))
-    monsters.add(monster)
-
-asters = sprite.Group()
-for i in range(1, 3):
-    aster = Aster(img_aster, randint(80, win_width - 80), -40, 80, 50, randint(1, 5))
-    asters.add(aster)
-
-bullets = sprite.Group()
+player_l = Player(img_hero_l, 15, win_height - 100, 15, 100, 10)
+player_r = Player(img_hero_r, 665, win_height - 100, 15, 100, 10)
 
 # переменная "игра закончилась": как только там True, в основном цикле перестают работать спрайты
 finish = False
@@ -125,91 +96,40 @@ while run:
         if e.type == QUIT:
             run = False
         # событие нажатия на пробел - спрайт стреляет
-        elif e.type == KEYDOWN:
-            if e.key == K_SPACE:
-                if reload == False:
-                    if num_fire < 20:
-                        fire_sound.play()
-                        ship.fire()
-                        num_fire += 1
-                    else:
-                        reload = True
-                        reloaded_time = timer()
+       
                 
-            
+
+    window.blit(background, (0,0))            
+    player_l.uprav_l()
+    player_r.uprav_r()
+    player_l.reset()
+    player_r.reset()
+    display.update()
+
+        
 
 
 
 
 
         
-    if not finish:
-        # обновляем фон
-        window.blit(background,(0,0))
-        sprites_list = sprite.groupcollide(monsters, bullets, True, True)
-        sprites_list2 = sprite.spritecollide(ship, monsters, True)
-        sprites_list3 = sprite.spritecollide(ship, asters, True)
+    
 
 
 
-        for monst in sprites_list:
-            monsterrr = Enemy(img_enemy,     randint(80, win_width - 80), -40, 80, 50, randint(1, 5))
-            monsters.add(monsterrr)
-            score += 1
-        if score >=30:
-            you_win = font2.render("You win! ",1, (255, 255, 255))
-            window.blit(you_win, (300, 250))
-            finish = True
-        if lost >=3:
-            you_lose = font2.render("You lose! ",1, (255, 255, 255))
-            window.blit(you_lose, (300, 250))
-            finish = True
-        if sprites_list2 != []:
-            life -= 1
-           
         
-        
-        if sprites_list3 != []:
-            life -=1   
-           
-
-        if life <=0:
-            you_lose = font2.render("You lose! ",1, (255, 255, 255))
-            window.blit(you_lose, (300, 250))
-            finish = True
-        
-        if reload == True:
-            now_time = timer()
-            if now_time - reloaded_time<3:
-                rel = font2.render("Подождите, идёт перезарядка ",1, (255, 255, 255))
-                window.blit(rel, (300, 400))
-            else:
-                reload = False
-                num_fire = 0
+   
          
 
         # пишем текст на экране
-        text = font2.render("Счет: " + str(score), 1, (255, 255, 255))
-        window.blit(text, (10, 20))
 
-        text_life = font2.render("Жизни: " + str(life), 1, (255, 0, 0))
-        window.blit(text_life, (580, 20))
-
-        text_lose = font2.render("Пропущено: " + str(lost), 1, (255, 255, 255))
-        window.blit(text_lose, (10, 50))
 
         # производим движения спрайтов
-        ship.update()
-        monsters.update()
-        bullets.update()
-        asters.update()
+       
 
         # обновляем их в новом местоположении при каждой итерации цикла
-        ship.reset()
-        monsters.draw(window)
-        bullets.draw(window)
-        asters.draw(window)
 
-        display.update()
+
+        
     # цикл срабатывает каждую 0.05 секунд
     time.delay(50)
